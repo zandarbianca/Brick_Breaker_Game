fc023903250c71f8c70939df5e373c49dc863343
Bianca Zandar <zandar.bianca@gmail.com>
2021-07-28 22:47:20 +0300

    First Commit

---
+++ Game_Brick_Breaker/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//background - an empty page with properties - JRrame object
+		JFrame obj = new JFrame();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+	}
+
+}
